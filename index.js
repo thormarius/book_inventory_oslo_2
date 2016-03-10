60688a4e8d780d221a00263e665648c31b5271a8
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-03-10 10:40:00 +0100

    express hello world

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function (req, res) {
-    res.end('hello world');
+app.get('/', function (req, res) {
+    res.send('Hello World!');
 });
 
-server.listen(3000, function () {
-    console.log("When this callback is invoked our server is listening on port: " + 3000);
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
 });
