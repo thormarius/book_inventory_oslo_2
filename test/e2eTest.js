01c307e02660f044cc3d57db8e1b4f38bb8db128
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-03-10 12:48:04 +0100

    e2e test

--- test/e2eTest.js
+++ test/e2eTest.js
@@ -1,3 +1,22 @@
-/**
- * Created by kwasniew on 10/03/16.
- */
+var request = require('supertest');
+var assert = require('assert');
+var app = require('../app.js');
+
+describe('Book inventory', function () {
+    it('allows to stock up the items', function (done) {
+        request(app).
+            post('/stock').
+            send({
+                "isbn": "1234567890",
+                "count": 10
+            }).
+            set('Content-Type', 'application/json').
+            expect('Content-Type', /json/).
+            expect(200).
+            end(function (err, res) {
+                if (err) return done(err);
+                assert.equal(res.body.isbn, "1234567890");
+                done();
+            });
+    })
+});
