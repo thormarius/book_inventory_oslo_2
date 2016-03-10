2218f7549972a583cd5970659623b76c3134c24a
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-03-10 15:35:30 +0100

    cleanup of routes and middleware

--- app.js
+++ app.js
@@ -1,63 +1,24 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var middleware = require('./middleware');
 
-var logIncoming = function (req, res, next) {
-    console.log('incoming request at ' + new Date());
-    next();
-};
 
-function clientError(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-}
-
-function serverError(err, req, res, next) {
-    res.status(err.status || 500);
-    console.error(err.stack);
-    res.json({
-        message: err.message,
-        error: (process.env.NODE_ENV === 'production') ? {} : err.stack
-    });
-}
 
 module.exports = function(stockRepository) {
     var app = express();
+    var routes = require('./routes')(stockRepository);
 
     app.use(bodyParser.json());
-    app.use(logIncoming);
+    app.use(middleware.logIncoming);
 
-    app.post('/stock', function (req, res, next) {
-        var isbn = req.body.isbn;
-        var count = req.body.count;
+    app.post('/stock', routes.stockUp);
 
-        stockRepository.
-            stockUp(isbn, count).
-            catch(next);
+    app.get('/stock', routes.findAll);
 
-        res.json({isbn: isbn, count: count});
-    });
+    app.get('/stock/:isbn', routes.getCount);
 
-    app.get('/stock', function (req, res, next) {
-        stockRepository.findAll().
-            then(function (docs) {
-                res.json(docs);
-            }).
-            catch(next);
-    });
-
-    app.get('/stock/:isbn', function (req, res) {
-        stockRepository.getCount(req.params.isbn).then(function (result) {
-            if (result !== null) {
-                res.status(200).json({count: result});
-            } else {
-                res.status(404).json({error: 'No book with ISBN: ' + req.params.isbn});
-            }
-        });
-    });
-
-    app.use(clientError);
-    app.use(serverError);
+    app.use(middleware.clientError);
+    app.use(middleware.serverError);
 
     return app;
 };
