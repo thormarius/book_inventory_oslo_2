f7a1cae7c3eec90f1e748152da3d1250e6739d35
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-03-10 10:38:22 +0100

    ignore deps

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("i am using vim");
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+    res.end('hello world');
+});
+
+server.listen(3000, function () {
+    console.log("When this callback is invoked our server is listening on port: " + 3000);
+});
