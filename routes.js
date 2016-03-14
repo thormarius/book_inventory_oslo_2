851827b6ab6fa64b558054bcec21bf717cd62ae1
thohenri <thor.marius.henrichsen@finn.no>
2016-03-14 10:08:01 +0100

    Content negotiation

--- routes.js
+++ routes.js
@@ -6,27 +6,50 @@ module.exports = function (stockRepository) {
             var count = req.body.count;
 
             stockRepository.
-                stockUp(isbn, count).
-                catch(next);
+            stockUp(isbn, count).
+            catch(next);
 
             res.json({isbn: isbn, count: count});
         },
         findAll: function (req, res, next) {
             stockRepository.findAll().
-                then(function (docs) {
-                    console.log("Retrieving all books - thor marius")
-                    res.json(docs);
-                }).
-                catch(next);
+            then(function (docs) {
+                console.log("Retrieving all books - thor marius")
+
+                res.json(docs);
+            }).
+            catch(next);
         },
         getCount: function (req, res) {
             stockRepository.getCount(req.params.isbn).then(function (result) {
                 if (result !== null) {
-                    console.log("Retrieving count " + result + " - thor marius")
-
-                    res.status(200).json({count: result});
+                    console.log("Retrieving count " + result + " - thor marius");
+                    res.format({
+                        "text/html": function () {
+                            res.status(200).send("<div style='color: red; font-weight: 'bold'>" + result + "</div>");
+                        },
+                        "application/json": function () {
+                            res.status(200).json({count: result});
+                        },
+                        "default": function () {
+                            res.status(406).send("Not acceptable")
+                        }
+                    });
                 } else {
-                    res.status(404).json({error: 'No book with ISBN: ' + req.params.isbn});
+                    console.log("Book with isbn: " + req.params.isbn);
+                    res.format({
+                        "text/html": function () {
+                            res.staus(200).send("<div style='color: red; font-weight: 'bold'> No book with ISBN: " + req.params.isbn + "</div>");
+                        },
+                        "application/json": function () {
+                            res.status(404).json({error: 'No book with ISBN: ' + req.params.isbn});
+                        },
+                        "default": function () {
+                            res.status(406).send("Not acceptable")
+                        }
+                    });
+
+
                 }
             });
         }
